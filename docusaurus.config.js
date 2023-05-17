3a8524a91f08dda74d8b5b8144ddae7d0e7e667b
ryan-mars <824046+ryan-mars@users.noreply.github.com>
2023-05-17 10:22:27 -0500

    meta tags

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "BDD Jumpstart",
+  title: "BDD Champions",
   tagline: "Get going with BDD!",
   favicon: "img/favicon.ico",
 
